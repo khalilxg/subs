7227019a98f368edadb46034cf4b64c885fd9fce
Med khalil bousnina <78953896+khalilxg@users.noreply.github.com>
2025-08-26 21:17:13 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,23 +2,17 @@ import Pricing from '@/components/ui/Pricing/Pricing';
 import { createClient } from '@/utils/supabase/server';
 import {
   getProducts,
-  getSubscription,
-  getUser
+
 } from '@/utils/supabase/queries';
 
 export default async function PricingPage() {
   const supabase = createClient();
-  const [user, products, subscription] = await Promise.all([
-    getUser(supabase),
+  const [ products] = await Promise.all([
     getProducts(supabase),
-    getSubscription(supabase)
   ]);
 
   return (
     <Pricing
-      user={user}
-      products={products ?? []}
-      subscription={subscription}
     />
   );
 }
