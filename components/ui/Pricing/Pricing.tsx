75bfb9fdb09ae1e90836741572ab01f4a2ab22f8
Med khalil bousnina <78953896+khalilxg@users.noreply.github.com>
2025-08-28 20:50:18 +0100

    Update Pricing.tsx

--- components/ui/Pricing/Pricing.tsx
+++ components/ui/Pricing/Pricing.tsx
@@ -122,7 +122,7 @@ export default function Pricing() {
                     : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
                 } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
               >
-                أفراد
+                 مواطنين أفراد
               </button>
               <button
                 onClick={() => setActivePlan('lawyers')}
@@ -133,7 +133,7 @@ export default function Pricing() {
                     : 'ml-0.5 relative w-1/2 border border-transparent text-zinc-400'
                 } rounded-md m-1 py-2 text-sm font-medium whitespace-nowrap focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-opacity-50 focus:z-10 sm:w-auto sm:px-8`}
               >
-                محامون وشركات
+                محامون أو شركات
               </button>
             </div>
 
